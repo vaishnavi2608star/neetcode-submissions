947a6f2e514792ef0917b126cfc38bca1d7a1149
vaishnavi2608star <stkbtcoe2480@kbtcoe.org>
2026-07-26 23:00:38 +0530

    Add: pow-x-n - submission-1

---
+++ Data Structures & Algorithms/pow-x-n/submission-1.java
@@ -0,0 +1,21 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if(n==0){
+            return 1;
+        }
+        if(n==1){
+            return x;
+        }
+        if(n==-1){
+            return 1/x;
+        }
+      double half=myPow(x,n/2);
+      if(n%2==0){
+        return half*half;
+      }else if(n>0){
+        return x*half*half;
+      }else{
+        return half*half/x;
+      }
+    }
+}
